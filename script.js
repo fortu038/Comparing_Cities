dbc00048ba52e4bf6d53608aeb494733480492b9
TreyKockelman <kockelmant@gmail.com>
2022-09-29 10:44:03 -0500

    added API Navigation

--- script.js
+++ script.js
@@ -4,7 +4,10 @@ function getAPI(city) {
     return response.json();
   })
   .then(function (data) {
-    console.log(data);
+    var country = data._links["city:country"].href;
+    var urbanArea = data._links["city:urban_area"].href;
+    getUA(urbanArea);
+    // getCountry(country);
   });
 }
 
@@ -14,8 +17,54 @@ function getCity(city) {
     return response.json();
   })
   .then(function (data) {
-    var geo = data._embedded["city:search-results"][0]._links["city:item"].href
+    var geo = data._embedded["city:search-results"][0]._links["city:item"].href;
     getAPI(geo);
+  });
+}
+
+function getUA(UA) {
+  fetch(UA)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    // console.log(data);
+    var scores = data._links["ua:scores"].href;
+    var details = data._links["ua:details"].href;
+    var salaries = data._links["ua:salaries"].href;
+    getUAscores(scores);
+    getUAdetails(details);
+    getUAsalaries(salaries);
+  });
+}
+
+function getUAscores(scores) {
+  fetch(scores)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+  });
+}
+
+function getUAdetails(details) {
+  fetch(details)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+  });
+}
+
+function getUAsalaries(salaries) {
+  fetch(salaries)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
   });
 }
 
